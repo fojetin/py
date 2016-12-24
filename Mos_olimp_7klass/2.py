b977cfc79c6a1bc47180742bf295c645b35db869
fojetin <fojetin@gmail.com>
2016-12-24 23:54:04 +0300

    Update and rename Mos_olimp_7/2.py to Mos_olimp_7klass/2.py

--- Mos_olimp_7klass/2.py
+++ Mos_olimp_7klass/2.py
@@ -4,10 +4,10 @@ t = list(map(int, input().split()))
 Ans = [0]
 j = 0
 for i in t:
-  if i>0:
-    Ans[j]+=1
-  else:
-    Ans.append(0)
-    j +=1
+    if i>0:
+        Ans[j]+=1
+    else:
+        Ans.append(0)
+        j +=1
 
 print(max(Ans))
