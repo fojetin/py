5595f0d4e1abc90a3993f91b4d320e11a63697a8
fojetin <fojetin@gmail.com>
2016-12-24 22:29:17 +0300

    Create 2.py

---
+++ Mos_olimp_7/2.py
@@ -0,0 +1,13 @@
+N = int(input()) #Прост чтобы было
+t = list(map(int, input().split()))
+
+Ans = [0]
+j = 0
+for i in t:
+  if i>0:
+    Ans[j]+=1
+  else:
+    Ans.append(0)
+    j +=1
+
+print(max(Ans))
